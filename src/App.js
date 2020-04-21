6259fe50482776916b2e6e21d0d137dadf6eca70
viveksingh-01 <viveksingh9301@gmail.com>
2020-04-21 22:35:38 +0530

    Add project code-base

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello World!</h1>
+      </React.Fragment>
+    );
+  }
+}
