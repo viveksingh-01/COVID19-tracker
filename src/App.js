9d52eccc6b8677b10e50019a0e42f1a87be9842f
viveksingh-01 <viveksingh9301@gmail.com>
2020-04-22 07:46:30 +0530

    Delete unnecessary files from the src

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default class App extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <h1>Hello World!</h1>
+        <h1>Welcome to COVID-19 Tracker App!</h1>
       </React.Fragment>
     );
   }
