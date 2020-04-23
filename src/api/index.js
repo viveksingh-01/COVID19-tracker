145e6b54dd16aea4fe30be0b7788352bf8808973
viveksingh-01 <viveksingh9301@gmail.com>
2020-04-23 23:26:18 +0530

    Update the fetchData method to filter the response

--- src/api/index.js
+++ src/api/index.js
@@ -4,8 +4,11 @@ const url = 'https://covid19.mathdro.id/api';
 
 const fetchData = async () => {
   try {
-    const data = await axios.get(url);
-    return data;
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate }
+    } = await axios.get(url);
+    const requiredData = { confirmed, recovered, deaths, lastUpdate };
+    return requiredData;
   } catch (error) {
     console.log(error);
   }
