ee69951fcee6218088512ef718a84ff550a6d021
viveksingh-01 <viveksingh9301@gmail.com>
2020-06-05 22:02:59 +0530

    Fetch daily-data in 'Chart' component

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { fetchDailyData } from './../../api';
 
-const Chart = () => {
-  return (
-    <div>
-      <h1>Chart</h1>
-    </div>
-  );
-};
+class Chart extends React.Component {
+  constructor() {
+    super();
+    this.state = { dailyData: [] };
+  }
+
+  async componentDidMount() {
+    const data = await fetchDailyData();
+    this.setState({
+      dailyData: data
+    });
+    console.log(this.state.dailyData);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Chart</h1>
+      </div>
+    );
+  }
+}
 
 export default Chart;
