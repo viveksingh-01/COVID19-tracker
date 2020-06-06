5178ae7feb55760676303daba995c0d73cea99cb
viveksingh-01 <viveksingh9301@gmail.com>
2020-06-07 01:06:57 +0530

    Use hooks to get and set countries list in 'CountryPicker'

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchCountries } from '../../services';
 
 const CountryPicker = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountryList = async () => {
+      setCountries(await fetchCountries());
+    };
+    getCountryList();
+  }, []);
+  console.log(countries);
+
   return (
     <div>
       <h1>CountryPicker</h1>
